2849654eefb20f7fb0cbdd85208c71e8f23d7e6a
1wsx10 <1wsx10@gmail.com>
2019-03-10 03:43:38 +1100

    start of assignment

---
+++ asdf.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+void plot(float x, float *y) {
+	*y = x * x*x;
+}
+
+void display() {
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+
+	//green
+	glColor3f(0.0f, 1.0f, 0.0f);
+	//x-axis
+	glBegin(GL_LINES);
+	glVertex3f(-1.0f, 0.0f, 0.1f);
+	glVertex3f(1.0f, 0.0f, 0.1f);
+
+	//red
+	glColor3f(1.0f, 0.0f, 0.0f);
+	//y-axis
+	glVertex3f(0.0f, -1.0f, 0.1f);
+	glVertex3f(0.0f, 1.0f, 0.1f);
+	glEnd();
+
+	//blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+
+	glBegin(GL_LINE_STRIP);
+	float y = 0;
+	for(float i = -1; i < 1.1f; i+= 0.1) {
+		plot(i, &y);
+		glVertex3f(i, y, 0.0f);
+	}
+	glEnd();
+
+	//glBegin(GL_POINTS);
+	//glColor3f(0.0f, 1.0f, 0.0f);
+	//glVertex2f(0.0f, 0.0f);
+	//glEnd();
+
+	//vsync
+	glutSwapBuffers();
+
+	// print out errors
+	int err;
+	while ((err = glGetError()) != GL_NO_ERROR) {
+		printf("display: %s\n", gluErrorString(err));
+	}
+
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch(key) {
+		case 27:
+		case 'q':
+			exit(EXIT_SUCCESS);
+			break;
+		default://every other key
+			break;
+	}
+}
+
+void init() {
+	glMatrixMode(GL_PROJECTION);
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("openGL hello world");
+
+	init();
+
+	glutDisplayFunc(display);
+	//glutDisplayFunc(keyboard);
+	glutMainLoop();
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -lGL -lGLU -lglut asdf.c -Wall -std=c99
