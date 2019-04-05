224f077dac52b9c8042b07cf7af34a6bed0143fa
s3543536 <s3543536@student.rmit.edu.au>
2019-04-06 03:42:01 +1100

    added better intersection for box and circle, added a demo car collision

--- main.c
+++ main.c
@@ -5,10 +5,32 @@ float scaley = 0.37f;
 float posx = 0.375f;
 float posy = 0.375f;
 
+char circle_point_is_intersect(circle *c, vector *point) {
+	return c->r > distance_vector(&c->c, point);
+}
+
 char circle_is_intersect(circle *c1, circle *c2) {
 	return c1->r + c2->r > distance_vector(&c1->c, &c2->c);
 }
 
+char circle_box_is_intersect2(circle *c, box *b) {
+	vector nearest_point;
+	nearest_point.z = 0;
+	if(c->c.x < b->c.x) {
+		nearest_point.x = fmax(c->c.x, b->c.x - b->w/2);
+	} else {
+		nearest_point.x = fmin(c->c.x, b->c.x + b->w/2);
+	}
+	if(c->c.y < b->c.y) {
+		nearest_point.y = fmax(c->c.y, b->c.y - b->h/2);
+	} else {
+		nearest_point.y = fmin(c->c.y, b->c.y + b->h/2);
+	}
+
+	return circle_point_is_intersect(c, &nearest_point);
+}
+
+// this is bad mkay
 char circle_box_is_intersect(circle *c, box *b) {
 	float bvr = b->h/2;//box vertical radius
 	float bhr = b->w/2;//box horizontal radius
@@ -323,6 +345,16 @@ void update(void) {
 		updateProjectileStateNumerical(&p);
 	}
 
+	//projectile collision
+	circle pj = {.r=0.05, .c={.x=p.pos.x, .y=p.pos.y, .z=p.pos.z}};
+	box firstcar = {.c=leveldata.cars[0].pos,.h=leveldata.cars[0].height, .w=CAR_WIDTH};
+	firstcar.c.y += firstcar.h/2;
+
+
+	if(circle_box_is_intersect2(&pj, &firstcar)) {
+		printf("%4.2f circle is intersecting\n", g.time);
+	}
+
 	// redraw the screen
 	glutPostRedisplay();
 }
@@ -345,9 +377,17 @@ void display() {
 
 	glColor3f(1, 1, 1);
 
+	circle pj = {.r=0.05, .c={.x=p.pos.x, .y=p.pos.y, .z=p.pos.z}};
+	draw_circle(&pj, 10, (char)0);
+
 	vector car_offset = {.x = 0.3, .y = -0.5, .z = 0};
 	vector car_scale = {.x = 0.2, .y = 0.5, .z = 0.2};
 	draw_car(1, car_offset, car_scale);
+	vector unitvec = {.x=1, .y=1, .z=1};
+
+	if(!is_init) {
+		draw_car(leveldata.cars[0].height, leveldata.cars[0].pos, unitvec);
+	}
 
 	vector triangle_pos = {.x=0, .y=0.5f, .z=0};
 	circle triangle = {.r=0.5f, .c=triangle_pos};
